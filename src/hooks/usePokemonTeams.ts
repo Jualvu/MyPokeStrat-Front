16744d8a570ed0a07da1e217b0ce88c0cd4c6dde
Julio Alvarado <julioandresalvaradoviquez@gmail.com>
2024-11-08 22:36:54 -0600

    prueba2

--- src/hooks/usePokemonTeams.ts
+++ src/hooks/usePokemonTeams.ts
@@ -1,20 +1,21 @@
-import { useEffect, useReducer } from "react"
+// import { useEffect, useReducer } from "react"
+import { useReducer } from "react"
 import { Action, PokemonInTeam, pokemonReducer } from "../utils/pokemonReducer"
 import Pokemon from "../types/PokemonType";
 import { v4 as uuidv4 } from 'uuid';
 
 const init = () => {
-    return JSON.parse( localStorage.getItem('pokemonTeam') || '{}') || [];
-    // return []
+    // return JSON.parse( localStorage.getItem('pokemonTeam') || '{}') || [];
+    return []
 }
 
 export const usePokemonTeams = () => {
     //pokemonTeam == state
     const [ pokemonTeam, dispatch] = useReducer(pokemonReducer, [], init);
     
-    useEffect(() => {
-        localStorage.setItem('pokemonTeam', JSON.stringify( pokemonTeam ) );
-      }, [pokemonTeam])
+    // useEffect(() => {
+    //     localStorage.setItem('pokemonTeam', JSON.stringify( pokemonTeam ) );
+    //   }, [pokemonTeam])
 
     const handleNewPokemon = ( pokemon: Pokemon ) => {
 
